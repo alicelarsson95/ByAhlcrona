c754eb23863c2c7554fba24f7fa7d97a3e647bb5
alicelarsson95 <alicelarsson95@hotmail.com>
2026-03-26 16:04:04 +0100

    Started on the navbar design

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,12 +3,22 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <h1 className={styles.navTitle}>Filippa Ahlcrona</h1>
-      <div className={styles.links}>
-        <a href="#portoflio">Portfolio</a>
-        <a href="#shop">Shop</a>
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a>
+      <div className={styles.container}>
+        <h1 className={styles.navTitle}>BY AHLCRONA</h1>
+        <div className={styles.links}>
+          <a className={styles.navLink} href="#portfolio">
+            PORTFOLIO
+          </a>
+          <a className={styles.navLink} href="#shop">
+            SHOP
+          </a>
+          <a className={styles.navLink} href="#about">
+            ABOUT
+          </a>
+          <a className={styles.navLink} href="#contact">
+            CONTACT
+          </a>
+        </div>
       </div>
     </nav>
   );
